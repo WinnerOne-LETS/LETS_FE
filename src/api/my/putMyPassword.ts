bd82cb1c1664e6e7d589479dc22fd05bf014ca3c
sunpaaaa <sunpaaaa@gmail.com>
2024-02-15 11:51:35 +0900

    Fix: 배포 에러 수정

--- src/api/my/putMyPassword.ts
+++ src/api/my/putMyPassword.ts
@@ -1,16 +1,13 @@
 const putMyPassword = async (password: string) => {
   try {
-    const result = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/v1/my/password`,
-      {
-        method: "PUT",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ password }),
+    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/v1/my/password`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify({ password }),
+    });
   } catch (error) {
     console.error(error);
     throw error;
